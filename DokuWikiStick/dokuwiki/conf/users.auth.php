606b6d0f53ca7b869c60b72777e43c4e4ecb38b3
unknown <ys8237@utexas.edu>
2018-05-04 20:36:19 -0500

    Added Project

---
+++ DokuWikiStick/dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+ym:$1$Eo4xsRc9$VEuE60NsS5167B9qRSWle1:YMS:yms4a2013@gmail.com:admin,user
